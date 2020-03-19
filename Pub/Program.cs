e273157c233327db6124b12acfd2e5505276b26f
castlewqc <wuqichao37@gamil.com>
2020-03-19 10:19:10 +0800

    添加项目文件。

---
+++ Pub/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("152.136.51.92:6379");
+            IDatabase db = redis.GetDatabase();
+            ISubscriber sub = redis.GetSubscriber();
+            sub.Publish("messages", "this is message");
+        }
+    }
+}
---
+++ Redis.Sample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redis.Sample
+{
+    using StackExchange.Redis;
+
+    class Program
+    {
+        //ServiceStack.Redis
+        //public static ServiceStack.Redis.RedisClient client 
+        //    = new ServiceStack.Redis.RedisClient("152.136.51.92", 6379);
+
+        static void Main(string[] args)
+        {
+            //多路调制器
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("152.136.51.92:6379");
+            IDatabase db = redis.GetDatabase();
+            db.StringSet("name", "_name");
+            db.StringSet("password", "_pwd");
+            var name = db.StringGet("name"); //RedisValue
+            string pwd = db.StringGet("password");
+
+            Console.WriteLine(string.Format("{0}-{1}", name, pwd));
+            ISubscriber sub = redis.GetSubscriber();
+            sub.Subscribe("messages", (channel, message) => {
+                Console.WriteLine(message);
+            });
+          
+            Console.ReadLine();
+        }
+    }
+}
